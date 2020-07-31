c4bbce82a9985df953007284481de624d6cc9aba
joegturner <59180847+joegturner@users.noreply.github.com>
2020-07-31 15:14:18 -0400

    revised readme

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Phrase Hunter - Word Guessing Game
- Word Guessing Game
-- Going for "Exceeds Expectations"
 
-## Authors
+## Author
+
 - Joe Turner / jgturner
 
 ## Revision History
+
 - Rev 0, 2-8-2020: submission for peer review on Slack
 
-## Going for "Exceeds Expectations"
-- Added keyboard functionality
-- Customized key 'chosen' and 'wrong' colors, changed phrase letter backgrounds to be circular 
+## Purpose
+
+- Try to guess the hidden phrase!
 
 ## Skills, Techniques, Process
-- Skills: Javascript and OOP (Object-Oriented Programming). HTML and CSS files were unedited from original copy
-- Techniques: included DOM manipulation, and Classes
 
+- JavaScript
+- OOP (Object-Oriented Programming)
+- DOM manipulation
--- js/Game.js
+++ js/Game.js
@@ -1,119 +1,119 @@
 class Game {
-    constructor () {
-        this.missed = 0;
-        this.phrases = [
-            new Phrase('Do or Do Not'),
-            new Phrase('You are Awesome'),
-            new Phrase('Avengers Assemble'),
-            new Phrase('I love New York'),
-            new Phrase('Pedro for president'),
-            new Phrase('Mi casa es su casa'),
-            new Phrase('Wash your hands'),
-            new Phrase('Do what you love'),
-            new Phrase('Love what you do'),
-            new Phrase('Who let the dogs out')
-        ];
-        this.activePhrase = null;
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      new Phrase("Do or Do Not"),
+      new Phrase("You are Awesome"),
+      new Phrase("Avengers Assemble"),
+      new Phrase("I love New York"),
+      new Phrase("Pedro for president"),
+      new Phrase("Mi casa es su casa"),
+      new Phrase("Wash your hands"),
+      new Phrase("Do what you love"),
+      new Phrase("Love what you do"),
+      new Phrase("Who let the dogs out"),
+    ];
+    this.activePhrase = null;
+  }
+
+  /**
+   * hides screen overlay
+   * chooses phrase and adds it to the board
+   */
+  startGame() {
+    document.getElementById("overlay").style.display = "none";
+    this.fetchQuote();
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+  }
+
+  /**
+   * randomy retrive a phrase
+   * @return {object}     phrase object
+   */
+  getRandomPhrase() {
+    return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  }
+
+  /**
+   * manages interaction between user input and board
+   */
+  handleInteraction(button, guess) {
+    // associate keydown key with onscreen key button
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].textContent === guess) {
+        button = buttons[i];
+      }
     }
 
-    /**
-     * hides screen overlay
-     * chooses phrase and adds it to the board
-     */
-    startGame() {
-        document.getElementById('overlay').style.display = 'none';
-        this.activePhrase = this.getRandomPhrase();
-        this.activePhrase.addPhraseToDisplay();
+    button.disabled = true;
+
+    if (this.activePhrase.checkLetter(guess)) {
+      button.className = "key chosen";
+      this.activePhrase.showMatchedLetter(guess);
+      if (this.checkForWin()) {
+        this.gameOver();
+      }
+    } else {
+      if (button.className != "key wrong") {
+        button.className = "key wrong";
+        this.removeLife();
+      }
     }
+  }
 
-    /**
-     * randomy retrive a phrase
-     * @return {object}     phrase object
-     */
-    getRandomPhrase() {
-        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  /**
+   * removes life from scoreboard
+   */
+  removeLife() {
+    const hearts = document.querySelectorAll(".tries");
+    hearts[this.missed].firstElementChild.src = "images/lostHeart.png";
+    this.missed += 1;
+    if (this.missed === 5) {
+      this.gameOver();
     }
+  }
 
-    /**
-     * manages interaction between user input and board
-     */
-    handleInteraction(button, guess) {
+  /**
+   * check to see if all letters are revealed
+   */
+  checkForWin() {
+    const phrase = this.activePhrase.letters;
 
-        // associate keydown key with onscreen key button
-        for (let i = 0; i < buttons.length; i++) {
-            if (buttons[i].textContent === guess) {
-                button = buttons[i];
-            }
+    for (let i = 0; i < phrase.length; i++) {
+      let status = phrase[i].className.substring(0, 4);
+      if (status === "hide") {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * ends game based on win or loss
+   */
+  gameOver() {
+    function resetOverlay() {
+      setTimeout(function () {
+        overlay.style.display = "";
+        while (oldPhrase.hasChildNodes()) {
+          oldPhrase.removeChild(oldPhrase.lastElementChild);
         }
-
-        button.disabled = true;
-
-        if (this.activePhrase.checkLetter(guess)){
-            button.className = 'key chosen';
-            this.activePhrase.showMatchedLetter(guess);
-            if (this.checkForWin()) {this.gameOver()}
-        } else {
-            if (button.className != 'key wrong') {
-                button.className = 'key wrong';
-                this.removeLife();
-            } 
-        }
+      }, 500);
     }
 
-    /**
-     * removes life from scoreboard
-     */
-    removeLife() {
-        const hearts = document.querySelectorAll('.tries');
-        hearts[this.missed].firstElementChild.src = 'images/lostHeart.png';
-        this.missed += 1;
-        if (this.missed === 5) {
-            this.gameOver();
-        }
+    const overlay = document.getElementById("overlay");
+    const h1 = document.getElementById("game-over-message");
+    const oldPhrase = document.querySelector("ul");
+
+    if (this.checkForWin()) {
+      overlay.className = "win";
+      h1.textContent = "Congratulations you are a Phrase Hunter!";
+      resetOverlay();
+    } else if (!this.checkForWin() && this.missed === 5) {
+      overlay.className = "lose";
+      h1.textContent = "You have lost :( Please try again";
+      resetOverlay();
     }
-
-    /**
-     * check to see if all letters are revealed
-     */
-    checkForWin() {
-        const phrase = this.activePhrase.letters;
-
-        for (let i = 0; i < phrase.length; i++) {
-            let status = phrase[i].className.substring(0, 4);
-            if (status === 'hide') {
-                return false;
-            } 
-        }
-        return true;
-    }
-
-    /**
-     * ends game based on win or loss
-     */
-    gameOver() {
-        
-        function resetOverlay() {
-            setTimeout(function() {
-                overlay.style.display = '';
-                while (oldPhrase.hasChildNodes()) {
-                    oldPhrase.removeChild(oldPhrase.lastElementChild);
-                }
-            }, 500);
-        }
-        
-        const overlay = document.getElementById('overlay');
-        const h1 = document.getElementById('game-over-message');
-        const oldPhrase = document.querySelector('ul');
-
-        if (this.checkForWin()) {
-            overlay.className = 'win';
-            h1.textContent = 'Congratulations you are a Phrase Hunter!';
-            resetOverlay();
-
-        } else if (!this.checkForWin() && this.missed === 5) {
-            overlay.className = 'lose';
-            h1.textContent = 'You have lost :( Please try again';
-            resetOverlay();
-        }    
-    }
+  }
 }
